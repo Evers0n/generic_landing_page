2a17ff1fc66df70e2d7fa49a51b4f0523f40c5fe
Everson Faria <everson.faria@grupoccaa.com.br>
2023-09-19 08:11:16 -0300

    cor_texto_dark_mode

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const checkbox = document.getElementById('checkbox')
 
 checkbox.addEventListener ('change', () => {
     document.body.classList.toggle('dark')
+    document.getElementsByClassName('lead').classList.toggle('lead-white')
 })
